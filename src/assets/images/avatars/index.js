f7faec1b2ed99f216cb6cac533636a0baf5b39c1
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-09-15 16:26:15 -0700

    Updated with new data model to be more efficient.

--- src/assets/images/avatars/index.js
+++ src/assets/images/avatars/index.js
@@ -1,64 +1,64 @@
 module.exports = {
-  avatars: [
-    {
+  avatars: {
+    1: {
       id: 1,
       src: require('./avatar_001.svg'),
       alt:
         'Boy superhero in a green costume with a yellow helmet, belt, and boots, and a square on his chest.',
     },
-    {
+    2: {
       id: 2,
       src: require('./avatar_002.svg'),
       alt:
         'Boy superhero with a red costume, cape, and mask, with brown hair, yellow boots, gloves, and belt, and a triangle on his chest.',
     },
-    {
+    3: {
       id: 3,
       src: require('./avatar_003.svg'),
       alt:
         'Boy superhero with a light blue costume, boots, and gloves, dark blue mask and cape, yellow belt, and circular symbol on his chest.',
     },
-    {
+    4: {
       id: 4,
       src: require('./avatar_004.svg'),
       alt:
         'Boy superhero in a black costume and mask with a blue cape, boots, gloves, and bottoms, and a lightning bolt on his chest.',
     },
-    {
+    5: {
       id: 5,
       src: require('./avatar_005.svg'),
       alt:
         'Boy superhero wearing a paper bag as a costume with a black mask, brown shorts, and a cooking pot on his head.',
     },
-    {
+    6: {
       id: 6,
       src: require('./avatar_006.svg'),
       alt:
         'Girl superhero with a pink and purple costume, skirt, mask, and boots, with a yellow circular symbol on her chest.',
     },
-    {
+    7: {
       id: 7,
       src: require('./avatar_007.svg'),
       alt:
         'Girl superhero with a light blue costume, mask, boots, and power fist gloves, and an arrow symbol on her chest.',
     },
-    {
+    8: {
       id: 8,
       src: require('./avatar_008.svg'),
       alt:
         'Girl superhero with a red costume and bandana mask, yellow boots, belt, and gloves, a jewelled circlet, and a yellow lightning bots on her neck.',
     },
-    {
+    9: {
       id: 9,
       src: require('./avatar_009.svg'),
       alt:
         'Girl superhero with a green costume and bandana mask, yellow gloves, cape, belt, and boots, and a diamond symbol on her chest.',
     },
-    {
+    10: {
       id: 10,
       src: require('./avatar_010.svg'),
       alt:
         'Girl superhero with a purple costume, a red cape and mask, yellow bottoms, massive power fists, and a triangle symbol on her chest.',
     },
-  ],
+  },
 };
